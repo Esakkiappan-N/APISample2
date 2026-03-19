e7ba824b92fbe34e6f6ba8e97fc7352425b5e2a4
Esakkiappan-N <esakki3124@gmail.com>
2026-03-19 22:54:18 +0530

    Latest commit

--- test_framework/conftest.py
+++ test_framework/conftest.py
@@ -4,11 +4,13 @@ from core.driver import get_driver
 from locators.web.variables import BASE_URL
 from pages.api.login_api import login_api, get_token    # ← fixed
 from utils.csv_reader import read_csv
-
+import os
 
 # ─────────────────────────────────────────
 # Mobile Fixture
 # ─────────────────────────────────────────
+
+
 @pytest.fixture(scope="function")
 def driver():
     driver = get_driver()
@@ -33,11 +35,23 @@ def auth_token():
 # Web Fixture
 # ─────────────────────────────────────────
 @pytest.fixture(scope="function")
-def page():
+def page(request):
     with sync_playwright() as p:
         browser = p.chromium.launch(
+<<<<<<< HEAD
             headless=True,
             args=["--no-sandbox","--disable-dev-shm-usage","--disable-gpu"]
+=======
+            headless=False,
+            args=["--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu"]
+        )
+
+        context = browser.new_context(viewport={"width": 1920, "height": 1080})
+        context.tracing.start(
+            screenshots=True,
+            snapshots=True,
+            sources=True
+>>>>>>> f7c7b63 (Latest commit)
         )
         context = browser.new_context(
             viewport={"width": 1920, "height": 1080}
@@ -46,6 +60,15 @@ def page():
         page.goto(BASE_URL)
 
         yield page
+
+        test_name = request.node.name
+        trace_dir = "trace-results"
+        os.makedirs(trace_dir, exist_ok=True)
+
+        trace_path = os.path.join(trace_dir, f"{test_name}.zip")
+
+        # ✅ STOP TRACE AND SAVE FILE
+        context.tracing.stop(path=trace_path)
 
         context.close()
         browser.close()
--- test_framework/core/driver.py
+++ test_framework/core/driver.py
@@ -4,14 +4,13 @@ from selenium.webdriver.common.options import ArgOptions
 
 def get_driver():
     options = ArgOptions()
-    options.set_capability("platformName", "Android")
-    options.set_capability("platformVersion", "15")
-    options.set_capability("deviceName", "0020965BT005049")
+    options.set_capability("app", "test_framework/apps/app-release-4-3-26.apk")
+    options.set_capability("deviceName", "emulator-5554")
     options.set_capability("automationName", "UiAutomator2")
     options.set_capability("appPackage", "com.whatsapp")
-    options.set_capability("appActivity", "com.whatsapp.HomeActivity")
+    options.set_capability("appActivity", "com.whatsapp.Main")
     options.set_capability("noReset", True)
 
-    driver = webdriver.Remote(
-        "http://127.0.0.1:4723/wd/hub", options=options)  # ← /wd/hub for v1
+    driver = webdriver.Remote("http://127.0.0.1:4723",
+                              options=options)  # ← /wd/hub for v1
     return driver
